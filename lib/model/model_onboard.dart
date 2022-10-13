70af14be843c8507b4729683c667acf443738d80
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2022-10-13 09:10:23 +0700

    add splash , onboard , welcome and Home with bloc

---
+++ lib/model/model_onboard.dart
@@ -0,0 +1,13 @@
+class OnBoarding {
+  final String imageUrl ;
+  final String detail ;
+
+  OnBoarding(this.imageUrl, this.detail);
+}
+
+// make data listnya
+List<OnBoarding> dataOnboarding = [
+  OnBoarding('asset/image_onboard.png', 'Kelola penjualanmu dengan lebih mudah dan praktis', ) ,
+  OnBoarding('asset/image_onboard.png', 'Investasikan bisnismu dengan aplikasi blanjaloka', ) ,
+  OnBoarding('asset/image_onboard.png', 'Investasi untung hari makin indah', ) ,
+] ;
