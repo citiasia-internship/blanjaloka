178fc954726bcad6820a928b3152271bc0699bdb
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-10-17 10:27:03 +0700

    detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:banjaloka/page/account_screen.dart';
 import 'package:banjaloka/page/agenda_screen.dart';
 import 'package:banjaloka/page/beranda_screen.dart';
+import 'package:banjaloka/page/detail_screen.dart';
 import 'package:banjaloka/page/home_page.dart';
 import 'package:banjaloka/page/market_modal_screen.dart';
 import 'package:banjaloka/page/on-board.dart';
@@ -21,23 +22,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     initialRoute: SplashScreen.routeName,
-      debugShowCheckedModeBanner: false,
-      routes: {
-        SplashScreen.routeName: (context) => SplashScreen(),
-        OnBoardingPage.routeName: (context) => OnBoardingPage(),
-        WelcomePage.routeName : (context) => const WelcomePage() ,
-        Home.routeName : (context) => const Home() ,
-        MarketModalScreen.routeName : (context) => const MarketModalScreen() ,
-        BerandaScreen.routeName : (context) => const BerandaScreen() ,
-        AgendaScreen.routeName : (context) => const AgendaScreen() ,
-        AccountScreen.routeName : (context) => const AccountScreen() ,
+        initialRoute: SplashScreen.routeName,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          SplashScreen.routeName: (context) => SplashScreen(),
+          OnBoardingPage.routeName: (context) => OnBoardingPage(),
+          WelcomePage.routeName: (context) => const WelcomePage(),
+          Home.routeName: (context) => const Home(),
+          MarketModalScreen.routeName: (context) => const MarketModalScreen(),
+          BerandaScreen.routeName: (context) => const BerandaScreen(),
+          AgendaScreen.routeName: (context) => const AgendaScreen(),
+          AccountScreen.routeName: (context) => const AccountScreen(),
+          DetailScreen.routeName: (context) => const DetailScreen(),
 
-        // register route
-        RegisterPage.routeName : (context) => const RegisterPage(),
-        RegisterPageSuccess.routeName : (context) => const RegisterPageSuccess(),
-      }
-    );
+          // register route
+          RegisterPage.routeName: (context) => const RegisterPage(),
+          RegisterPageSuccess.routeName: (context) =>
+              const RegisterPageSuccess(),
+        });
   }
 }
-
--- lib/widget/content_home.dart
+++ lib/widget/content_home.dart
@@ -1,6 +1,7 @@
 import 'package:banjaloka/bloc/business_event.dart';
 import 'package:banjaloka/bloc/business_state.dart';
 import 'package:banjaloka/model/model_item_segera.dart';
+import 'package:banjaloka/page/detail_screen.dart';
 import 'package:banjaloka/respository/bussines_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,36 +33,29 @@ class ContentHome extends StatelessWidget {
               children: [
                 buildTitle('Pendanaan Berlangsung'),
                 Container(
-                  margin: const EdgeInsets.only(
-                      top: 15,
-                      left: 25
-                  ),
+                  margin: const EdgeInsets.only(top: 15, left: 25),
                   height: 200,
                   child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
+                      scrollDirection: Axis.horizontal,
                       itemCount: business.length,
                       itemBuilder: (context, index) {
                         return Padding(
-                          padding: const EdgeInsets.only(
-                            right: 15
-                          ),
+                          padding: const EdgeInsets.only(right: 15),
                           child: buildContent(
+                              context,
                               business[index].image,
                               business[index].name,
                               business[index].price,
                               '50 %',
                               true,
-                              0.5 ),
-                        ) ;
+                              0.5),
+                        );
                       }),
                 ),
                 const SizedBox(height: 30),
                 buildTitle('Segera Dibuka'),
                 Container(
-                  margin: const EdgeInsets.only(
-                    top: 15,
-                    left: 25
-                  ),
+                  margin: const EdgeInsets.only(top: 15, left: 25),
                   height: 200,
                   child: ListView.builder(
                       scrollDirection: Axis.horizontal,
@@ -70,21 +64,22 @@ class ContentHome extends StatelessWidget {
                         return Padding(
                           padding: EdgeInsets.only(right: 15),
                           child: buildContent(
+                              context,
                               business[index].image,
                               business[index].name,
                               business[index].price,
                               null,
                               null,
                               null),
-                        ) ;
+                        );
                       }),
                 ),
               ],
             );
-          } else if (state is BusinessErrorState){
-            return Text('error ') ;
+          } else if (state is BusinessErrorState) {
+            return Text('error ');
           } else {
-            return Container() ;
+            return Container();
           }
         },
       ),
@@ -110,70 +105,79 @@ class ContentHome extends StatelessWidget {
     );
   }
 
-  Widget buildContent(String imgUrl, String title, String price,
-      String? percenBussines, bool? progresIndicator, double? valueBussines) {
-    return Container(
-      height: 200,
-      width: 149,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        elevation: 2,
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(8), topRight: Radius.circular(8)),
-              child: Image.network(
-                imgUrl,
-                height: 80,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-            // detail dari item
-            Expanded(
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 9.5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title, style: titleItemCard),
-                    SizedBox(height: 10),
-                    progresIndicator == true
-                        ? LinearProgressIndicator(
-                            backgroundColor: progressGrey,
-                            valueColor: const AlwaysStoppedAnimation<Color>(
-                                Color(0xff398AB9)),
-                            value: valueBussines,
-                          )
-                        : Container(),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Nilai Bisnis', style: businessPriceItemCard),
-                          percenBussines != null
-                              ? Text(
-                                  percenBussines,
-                                  style: businessPriceItemCard,
-                                )
-                              : Container()
-                        ]),
-                   const  SizedBox(
-                      height: 6,
-                    ),
-                    Text(
-                      'Rp ${price}',
-                      style: priceItemCard,
-                    )
-                  ],
+  Widget buildContent(
+      BuildContext context,
+      String imgUrl,
+      String title,
+      String price,
+      String? percenBussines,
+      bool? progresIndicator,
+      double? valueBussines) {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, DetailScreen.routeName),
+      child: Container(
+        height: 200,
+        width: 149,
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          elevation: 2,
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                child: Image.network(
+                  imgUrl,
+                  height: 80,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
                 ),
               ),
-            )
-          ],
+              // detail dari item
+              Expanded(
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 9.5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title, style: titleItemCard),
+                      SizedBox(height: 10),
+                      progresIndicator == true
+                          ? LinearProgressIndicator(
+                              backgroundColor: progressGrey,
+                              valueColor: const AlwaysStoppedAnimation<Color>(
+                                  Color(0xff398AB9)),
+                              value: valueBussines,
+                            )
+                          : Container(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Nilai Bisnis', style: businessPriceItemCard),
+                            percenBussines != null
+                                ? Text(
+                                    percenBussines,
+                                    style: businessPriceItemCard,
+                                  )
+                                : Container()
+                          ]),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        'Rp ${price}',
+                        style: priceItemCard,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
