f93bc676cd5f0c8cf74d7a8f79b1018a5f224a87
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-10-17 11:02:44 +0700

    update detail page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: SplashScreen.routeName,
+        initialRoute: DetailScreen.routeName,
         debugShowCheckedModeBanner: false,
         routes: {
           SplashScreen.routeName: (context) => SplashScreen(),
