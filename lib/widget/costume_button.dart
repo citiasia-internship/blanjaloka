a8a11b45cdaface5f4b96ae73cf02b419aab9133
fadillaoegi <fadilfdl2710@gmail.com>
2022-11-02 13:12:28 +0700

    Update profile redisign

--- lib/widget/costume_button.dart
+++ lib/widget/costume_button.dart
@@ -1,29 +1,29 @@
 import 'package:banjaloka/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-Widget costumeButtonFill(String title){
+Widget costumeButtonFill(String title) {
   return Container(
     width: 323,
     height: 48,
     decoration: BoxDecoration(
-      color: primaryBlue6,
-      borderRadius: BorderRadius.circular(10)
-    ),
+        color: primaryBlue6, borderRadius: BorderRadius.circular(10)),
     child: Center(
-      child: Text(title, style: textButton,),
+      child: Text(
+        title,
+        style: textButton,
+      ),
     ),
   );
 }
 
-Widget   costumeButtonBorder(String title){
+Widget costumeButtonBorder(String title) {
   return Container(
     width: 323,
     height: 48,
     decoration: BoxDecoration(
-      color: neutralWhite,
-      borderRadius: BorderRadius.circular(10),
-      border: Border.all(color: primaryBlue6,width: 1)
-    ),
+        color: neutralWhite,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: primaryBlue6, width: 1)),
     child: Center(
       child: Text(title, style: textButton.copyWith(color: primaryBlue6)),
     ),
